576d4e8d055b2e4d42885c170d34d1f6b48a265f
朴成佑 <sungwoop0403@yahoo.co.jp>
2017-09-25 16:31:03 +0900

    no message

--- practice/practice.py
+++ practice/practice.py
@@ -19,7 +19,7 @@ class Status:
         self.friend = friend
         self.enemy = enemy
     def Attack(self,target):
-        damage = int((self.attack * random.uniform(0.7,1.3)) - target.defence * random.uniform(0.9,1.1))
+        damage = int((self.attack * random.uniform(0.8,1.2)) - target.defence * random.uniform(0.9,1.1))
         target.hitpoint -= damage
         if choice == 0:
             print('{}が{}に通常攻撃--{}のダメージ'.format(self.NAME,target.NAME,damage))
@@ -29,7 +29,7 @@ class Status:
         self.movecheck = True
     def Skill1(self,target):
         if(self.magicpoint >= 10):
-            damage = int(self.attack * random.uniform(0.7,1.3) * 1.4 - target.defence * random.uniform(0.9,1.1))
+            damage = int(self.attack * random.uniform(0.8,1.2) * 1.4 - target.defence * random.uniform(0.9,1.1))
             target.hitpoint -= damage
             if choice == 0:
                 print('{}が{}にスキル攻撃--{}のダメージ'.format(self.NAME,target.NAME,damage))
@@ -50,17 +50,16 @@ class Status:
     def Choice(self):
         st1 = int(CheckStatus(player,friend,enemy),4)
         t = 0
-        for y in data[st1]:
+        for y in windata[st1]:
             t += y
         ra = random.randrange(t)
-        if ra < data[st1][0]:
+        if ra < windata[st1][0]:
             temp = 0
-        elif ra < data[st1][0] + data[st1][1]:
+        elif ra < windata[st1][0] + windata[st1][1]:
             temp = 1
-        elif ra < data[st1][0] + data[st1][1] + data[st1][2]:
+        elif ra < windata[st1][0] + windata[st1][1] + windata[st1][2]:
             temp = 2
         return temp
-
     def Action(self):
         if self.live == True:
             st1 = int(CheckStatus(player,friend,enemy),4)
@@ -73,7 +72,7 @@ class Status:
                 _number = input('0:通常攻撃,1:スキル攻撃,2:自己回復 >>')
                 number = int(_number)
             elif self.enemy == True:
-                number = 0
+                number = self.EnemyAction()
             if self.friend == True or self.player == True:
                 target = _list[2]
             elif self.enemy == True:
@@ -92,6 +91,15 @@ class Status:
             if self.NAME == 'NPC':
                 listSt.append(st1)
                 listNm.append(number)
+    def EnemyAction(self):
+        if self.magicpoint >= 10:
+            if self.hitpoint < self.MAXHITPOINT / 2:
+                temp = 2
+            elif self.hitpoint >= self.MAXHITPOINT / 2:
+                temp = random.randrange(3)
+        elif self.magicpoint < 10:
+            temp = 0
+        return temp
 def Qsort(li):
     if len(li) < 2:
         return li
@@ -173,12 +181,13 @@ def Flow():
     if enemy.live == True:
         for x in listSt:
             for y in listNm:
-                if(data[x][y] > 1):
-                    data[x][y] -= 1
+                if(windata[x][y] > 2):
+                    windata[x][y] -= 1
+                    losedata[x][y] += 1
     elif enemy.live == False:
         for x in listSt:
             for y in listNm:
-                data[x][y] += 1
+                windata[x][y] += 1
     return
 
 def FlowTest():
@@ -198,29 +207,34 @@ def FlowTest():
 
 
 def DataSave():
-    np.save('data.npy',data)
-    np.savetxt('data.csv',data)
+    np.savez('data.npy',_windata = windata,_losedata = losedata)
+    np.savetxt('windata.csv',windata)
+    np.savetxt('losedata.csv',losedata)
+
 def DataRead():
     try:
         data = np.load('data.npy')
+        windata = data['_windata']
+        losedata = data['_losedata']
     except IOError:
-        data = np.ones((4096,3))
-    return data
+        windata = np.ones((4096,3))
+        losedata = np.ones((4096,3))
+    return [windata,losedata]
 
 
-data = DataRead()
-COUNT = 100000
+windata,losedata = DataRead()
+COUNT = 1000
 n = 0
 win = 0
 
 choice = int(input('0 : 学習結果 , 1 : 学習 >>'))
 if choice == 0:
     #自キャラ
-    player = Status('Player',80,40,40,15)
+    player = Status('Player',80,40,60,15)
     #味方AI
-    friend = Status('NPC',120,50,30,10,False,True)
+    friend = Status('NPC',120,40,50,10,False,True)
     #敵キャラ
-    enemy = Status('Enemy',350,60,30,20,False,False,True)
+    enemy = Status('Enemy',320,50,30,20,False,False,True)
     _list = [player,friend,enemy]
     listSt = []
     listNm = []
@@ -231,13 +245,13 @@ if choice == 0:
         print("LOSE")
 elif choice == 1:
     while(n < COUNT):
-        print("{}回目".format(n + 1))
+        #print("{}回目".format(n + 1))
         #自キャラ
-        player = Status('Player',80,40,40,15,False,True)
+        player = Status('Player',80,40,50,15,False,True)
         #味方AI
         friend = Status('NPC',120,50,30,10,False,True)
         #敵キャラ
-        enemy = Status('Enemy',350,60,30,20,False,False,True)
+        enemy = Status('Enemy',380,60,31,20,False,False,True)
         _list = [player,friend,enemy]
         listSt = []
         listNm = []
